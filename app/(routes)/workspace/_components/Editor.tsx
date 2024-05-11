6d45f06566d1bdc0207a8fb4e26bafcf0b7ee30b
Aniket Dhaygude <aniketdhaygude4@gmail.com>
2024-05-11 12:47:03 +0530

    Save Document Data only

--- app/(routes)/workspace/_components/Editor.tsx
+++ app/(routes)/workspace/_components/Editor.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import EditorJS from '@editorjs/editorjs';
 // @ts-ignore
 import Header from '@editorjs/header';
@@ -10,16 +10,48 @@ import List from "@editorjs/list";
 import Checklist from '@editorjs/checklist';
 // @ts-ignore
 import Paragraph from '@editorjs/paragraph';
+import { useMutation } from 'convex/react';
+import { api } from '@/convex/_generated/api';
+import { toast } from 'sonner';
+
+const rawDocument={
+    "time" : 1550476186479,
+    "blocks" : [{
+        data:{
+            text:'Document Name',
+            level:2
+        },
+        id:"123",
+        type:'header'
+    },
+    {
+        data:{
+            level:4
+        },
+        id:"1234",
+        type:'header'
+    }],
+    "version" : "2.8.1"
+}
 
 
-function Editor() {
+function Editor({onSaveTrigger,fileId}:any) {
 
     const ref=useRef<EditorJS>();
+    const updateDocument=useMutation(api.files.updateDocument);
+    const [document,setDocument]=useState(rawDocument);
 
     useEffect(()=>{
         initEditor();
     },[])
      
+    useEffect(()=>{
+        console.log("Trigger Value:",onSaveTrigger);
+        onSaveTrigger&&onSaveDocument(); 
+
+    },[onSaveTrigger])
+
+
     const initEditor=()=>{
             const editor = new EditorJS({
             /**
@@ -49,11 +81,33 @@ function Editor() {
                     inlineToolbar: true,
                   },
             },
-            holder: 'editorjs'
+            holder: 'editorjs',
+            data:document
             });
 
             ref.current=editor;
         
+    }
+
+    const onSaveDocument=()=>{
+        if(ref.current)
+            {
+                ref.current.save().then((outputData) => {
+                    console.log('Article data: ', outputData)
+                     updateDocument({
+                        _id:fileId,
+                        document:JSON.stringify(outputData) 
+                     }).then(resp=>{
+                
+                         toast("Document Upadted!")
+                        
+                     },(e)=>{
+                        toast("Server Error")
+                     })
+                  }).catch((error) => {
+                    console.log('Saving failed: ', error)
+                  });
+            }
     }
   return (
     <div>
--- app/(routes)/workspace/_components/WorkspaceHeader.tsx
+++ app/(routes)/workspace/_components/WorkspaceHeader.tsx
@@ -1,9 +1,9 @@
 import { Button } from '@/components/ui/button'
-import { Link } from 'lucide-react'
+import { Link, Save } from 'lucide-react'
 import Image from 'next/image'
 import React from 'react'
 
-function WorkspaceHeader() {
+function WorkspaceHeader({onSave}:any) {
   return (
     <div className='p-3 border-b flex justify-between'>
         <div className='flex gap-2 items-center'>
@@ -13,10 +13,16 @@ function WorkspaceHeader() {
             />
             <h2>File Name</h2>
         </div>
+        <div className='flex items-center gap-4'>
+          <Button className='h-8 text-[12px] gap-2 bg-yellow-500 hover:bg-yellow-700'
+          onClick={()=>onSave()}>
+          <Save className='h-4 w-4'/> save
+          </Button>
         <Button className='h-8 text-[12px] gap-2 bg-blue-500 hover:bg-blue-700'>Share
         <Link className='h-4 w-4'/>
         </Button>
-         
+        </div>
+
     </div>
   )
 }
